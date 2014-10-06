0e2353356362c8e0c42ac179c79872e20c69818d
Alexey Melnichuk <mimir@newmail.ru>
2014-10-06 11:39:15 +0500

    Fix. install gyp on travis.

--- .travis/setup_uv.sh
+++ .travis/setup_uv.sh
@@ -8,6 +8,9 @@ git clone https://github.com/joyent/libuv.git
 
 cd libuv
 
-./gyp_uv.py -f make && BUILDTYPE=Release CFLAGS=-fPIC ${MAKE} -C out
+mkdir -p build
+git clone https://git.chromium.org/external/gyp.git build/gyp
+
+./gyp_uv.py -f make && BUILDTYPE=Release CFLAGS=-fPIC make -C out
 
 cd $TRAVIS_BUILD_DIR
