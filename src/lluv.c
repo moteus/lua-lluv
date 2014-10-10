2ccbceada90eab739838d54b22a18086fd370c4a
Alexey Melnichuk <mimir@newmail.ru>
2014-10-10 10:40:15 +0500

    Fix. All modules use same library registry.

--- src/lluv.c
+++ src/lluv.c
@@ -44,6 +44,8 @@ static int luaopen_lluv_impl(lua_State *L, int safe){
   if(!lua_istable(L, -1)){ /* registry */
     lua_pop(L, 1);
     lua_newtable(L);
+    lua_pushvalue(L, -1);
+    lua_rawsetp(L, LUA_REGISTRYINDEX, LLUV_REGISTRY);
   }
 
   lua_newtable(L); /* library  */
