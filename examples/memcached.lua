094361ec0edc0d9160c87793d3f41d2e01dcaade
Alexey Melnichuk <mimir@newmail.ru>
2014-10-16 16:18:10 +0500

    Update memcached example.

--- examples/memcached.lua
+++ examples/memcached.lua
@@ -39,6 +39,7 @@ local SERVER_ERRORS = {
 
 local STORE_RESP = {
   STORED        = true;
+  DELETED       = true;
   NOT_STORED    = true;
   EXISTS        = true;
   NOT_EXISTS    = true;
@@ -98,6 +99,19 @@ local function make_retr(cmd, key)
   return cmd .. " " .. key .. EOL
 end
 
+local function make_change(cmd, key, noreply)
+  assert(cmd)
+  assert(key)
+  return cmd .. " " .. key .. (noreply and " noreply" or "") .. EOL
+end
+
+local function make_inc(cmd, key, value, noreply)
+  assert(cmd)
+  assert(key)
+  assert(value)
+  return cmd .. " " .. key .. " " .. value .. (noreply and " noreply" or "") .. EOL
+end
+
 -------------------------------------------------------------------
 local Connection = {} do
 Connection.__index = Connection
@@ -119,14 +133,19 @@ function Connection:connected()
 end
 
 function Connection:open(cb)
-  if self:connected() then return ocall(cb) end
+  if self:connected() then return ocall(cb, self) end
+
   return uv.tcp():connect(self._host, self._port, function(cli, err)
     if err then
       cli:close()
-      return ocall(cb, err)
+      return ocall(cb, self, err)
     end
+
     cli.data = self
     self._cnn = cli
+    self._buff.reset()
+    self._queue.reset()
+
     cli:start_read(function(cli, err, data)
       if err then
         self:close()
@@ -134,7 +153,8 @@ function Connection:open(cb)
       end
       return self:_read(data)
     end)
-    return ocall(cb)
+
+    return ocall(cb, self)
   end)
 end
 
@@ -146,6 +166,8 @@ end
 
 function Connection:on_error(err) end
 
+local WAIT = {}
+
 function Connection:_read(data)
   local req = self._queue.peek()
   if not req then -- unexpected reply
@@ -153,78 +175,77 @@ function Connection:_read(data)
     return ocall(self.on_error, self, Error:new("Protocol error"))
   end
 
+  self._buff.append(data)
+
   while req do
     if req.type == REQ_STORE then
-      local line = self._buff.next_line(data)
-      if not line then return end
-      assert(self._queue.pop() == req)
-
-      if STORE_RESP[line] then
-        ocall(req.cb, nil, line)
-      else
-        local res, value = split_first(line, " ", true)
-        if SERVER_ERRORS[res] then
-          ocall(req.cb, Error:new(line))
-        else
-          self:close()
-          return ocall(self.on_error, self, Error:new("Protocol error"))
-        end
-      end
-      
+      local ret = self:_on_store(req)
+      if ret == WAIT then return end
     elseif req.type == REQ_RETR then
-
-      if not req.len then -- we wait next data
-        local line = self._buff.next_line(data)
-        if not line then return end
-        data = ""
-
-        if line == "END" then -- no more data
-          assert(self._queue.pop() == req)
-
-          if req.multi then
-            ocall(req.cb, nil, req.res)
-          elseif not req.res then -- no data
-            ocall(req.cb, nil, nil)
-          else
-            ocall(req.cb, nil, req.res[1].data, req.res[1].flags, req.res[1].cas)
-          end
-
-          req = nil
-        else
-          local res, key, flags, len, cas = usplit(line, " ", true)
-          if res == "VALUE" then
-            req.key   = key
-            req.len   = tonumber(len) + #EOL
-            req.flags = tonumber(flags) or 0
-            req.cas   = tonumber(cas)
-          elseif SERVER_ERRORS[res] then
-            assert(self._queue.pop() == req)
-            ocall(req.cb, Error:new(line))
-            req = nil
-          else
-            self:close()
-            return ocall(self.on_error, self, Error:new("Protocol error"))
-          end
-        end
-      end
-
-      if req then -- we need next chunk of data
-        assert(req.len)
-
-        local data = self._buff.next_n(data, req.len)
-        if not data then return end
-        
-        if not req.res then req.res = {} end
-        req.res[#req.res + 1] = { data = string.sub(data, 1, -3); flags = req.flags; cas = req.cas; key = req.key; }
-        req.len = nil
-      end
-
+      local ret = self:_on_retr(req)
+      if ret == WAIT then return end
     else
       assert(false, "unknown request type :" .. tostring(req.type))
     end
 
-    data, req = "", self._queue.peek()
+    req = self._queue.peek()
   end
+end
+
+function Connection:_on_store(req)
+  local line = self._buff.next_line()
+  if not line then return WAIT end
+  assert(self._queue.pop() == req)
+
+  if STORE_RESP[line] then
+    return ocall(req.cb, self, nil, line)
+  end
+
+  local res, value = split_first(line, " ", true)
+  if SERVER_ERRORS[res] then
+    return ocall(req.cb, self, Error:new(line))
+  end
+
+  -- for increment/decrement line is just data
+  return ocall(req.cb, self, nil, line)
+end
+
+function Connection:_on_retr(req)
+  if not req.len then -- we wait next value
+    local line = self._buff.next_line()
+    if not line then return WAIT end
+
+    if line == "END" then -- no more data
+      assert(self._queue.pop() == req)
+
+      if req.multi then   return ocall(req.cb, self, nil, req.res) end
+      if not req.res then return ocall(req.cb, self, nil, nil) end
+      return ocall(req.cb, self, nil, req.res[1].data, req.res[1].flags, req.res[1].cas)
+    end
+
+    local res, key, flags, len, cas = usplit(line, " ", true)
+    if res == "VALUE" then
+      req.key   = key
+      req.len   = tonumber(len) + #EOL
+      req.flags = tonumber(flags) or 0
+      req.cas   = cas ~= "" and cas or nil
+    elseif SERVER_ERRORS[res] then
+      assert(self._queue.pop() == req)
+      return ocall(req.cb, self, Error:new(line))
+    else
+      self:close()
+      return ocall(self.on_error, self, Error:new("Protocol error"))
+    end
+  end
+
+  assert(req.len)
+
+  local data = self._buff.next_n(nil, req.len)
+  if not data then return WAIT end
+  
+  if not req.res then req.res = {} end
+  req.res[#req.res + 1] = { data = string.sub(data, 1, -3); flags = req.flags; cas = req.cas; key = req.key; }
+  req.len = nil
 end
 
 function Connection:_send(data, type, cb)
@@ -239,11 +260,30 @@ function Connection:_send(data, type, cb)
   return self
 end
 
--- (key, data, [[[exptime,] flags,] noreply])
-function Connection:set(...)
+-- (key, data, [exptime[, flags[, noreply]]])
+function Connection:_set(cmd, ...)
   local cb, key, data, exptime, flags, noreply = cb_args(...)
   return self:_send(
-    make_store("set", key, data, exptime, flags, noreply),
+    make_store(cmd, key, data, exptime, flags, noreply),
+    REQ_STORE, cb
+  )
+end
+
+function Connection:set(...)     return self:_set("set", ...)     end
+
+function Connection:add(...)     return self:_set("add", ...)     end
+
+function Connection:replace(...) return self:_set("replace", ...) end
+
+function Connection:append(...)  return self:_set("append", ...)  end
+
+function Connection:prepend(...) return self:_set("prepend", ...) end
+
+-- (key, data, [exptime[, flags[, noreply]]])
+function Connection:cas(...)
+  local cb, key, data, cas, exptime, flags, noreply = cb_args(...)
+  return self:_send(
+    make_store("cas", key, data, exptime, flags, noreply, cas),
     REQ_STORE, cb
   )
 end
@@ -255,10 +295,189 @@ function Connection:get(key, cb)
   )
 end
 
+function Connection:gets(key, cb)
+  return self:_send(
+    make_retr("gets", key),
+    REQ_RETR, cb
+  )
+end
+
+-- (key[, noreply])
+function Connection:delete(...)
+  local cb, key, noreply = cb_args(...)
+  return self:_send(
+    make_change("delete", key, noreply),
+    REQ_STORE, cb
+  )
+end
+
+-- (key[, value[, noreply]])
+function Connection:increment(...)
+  local cb, key, value, noreply = cb_args(...)
+  value = value or 1
+  return self:_send(
+    make_inc("incr", key, value, noreply),
+    REQ_STORE, cb
+  )
+end
+
+-- (key[, value[, noreply]])
+function Connection:decrement(...)
+  local cb, key, value, noreply = cb_args(...)
+  value = value or 1
+  return self:_send(
+    make_inc("decr", key, value, noreply),
+    REQ_STORE, cb
+  )
+end
+
+-- (key, value[, noreply])
+function Connection:touch(...)
+  local cb, key, value, noreply = cb_args(...)
+  assert(value)
+  return self:_send(
+    make_inc("touch", key, value, noreply),
+    REQ_STORE, cb
+  )
+end
+
 end
 -------------------------------------------------------------------
+
+local function self_test(server, key)
+  key = key or "test_key"
+
+  Connection:new(server):open(function(self, err)
+    assert(not err, tostring(err))
+
+    function self:on_error(err)
+      assert(false, tostring(err))
+    end
+
+    self:delete(key)
+
+    uv.run("once")
+
+    self:delete(key, function(self, err, ret)
+      assert(not err, tostring(err))
+      assert(ret == "NOT_FOUND", tostring(ret))
+    end)
+
+    uv.run("once")
+
+    self:increment(key, 5, function(self, err, ret)
+      assert(not err, tostring(err))
+      assert(ret == "NOT_FOUND", tostring(ret))
+    end)
+
+    uv.run("once")
+
+    self:get(key, function(self, err, ret)
+      assert(not err, tostring(err))
+      assert(ret == nil, tostring(ret))
+    end)
+
+    uv.run("once")
+
+    self:replace(key, "hello", function(self, err, ret)
+      assert(not err, tostring(err))
+      assert(ret == "NOT_STORED", tostring(ret))
+    end)
+
+    uv.run("once")
+
+    self:append(key, "hello", function(self, err, ret)
+      assert(not err, tostring(err))
+      assert(ret == "NOT_STORED", tostring(ret))
+    end)
+
+    uv.run("once")
+
+    self:prepend(key, "hello", function(self, err, ret)
+      assert(not err, tostring(err))
+      assert(ret == "NOT_STORED", tostring(ret))
+    end)
+
+    uv.run("once")
+
+    self:set(key, "72", 0, 12, function(self, err, ret)
+      assert(not err, tostring(err))
+      assert(ret == "STORED", tostring(ret))
+    end)
+
+    uv.run("once")
+
+    self:get(key, function(self, err, ret, flags, cas)
+      assert(not err, tostring(err))
+      assert(ret   == "72", tostring(ret))
+      assert(flags == 12,   tostring(flags))
+      assert(cas   == nil,  tostring(cas))
+    end)
+
+    uv.run("once")
+
+    self:gets(key, function(self, err, ret, flags, cas)
+      assert(not err, tostring(err))
+      assert(ret   == "72", tostring(ret))
+      assert(flags == 12,   tostring(flags))
+      assert(type(cas) == "string",  type(cas) .. " - " .. tostring(cas))
+    end)
+
+    uv.run("once")
+
+    self:add(key, "hello", function(self, err, ret)
+      assert(not err, tostring(err))
+      assert(ret == "NOT_STORED", tostring(ret))
+    end)
+
+    uv.run("once")
+
+    self:increment(key, 5, function(self, err, ret)
+      assert(not err, tostring(err))
+      assert(ret == "77", tostring(ret))
+    end)
+
+    uv.run("once")
+
+    self:decrement(key, 2, function(self, err, ret)
+      assert(not err, tostring(err))
+      assert(ret == "75", tostring(ret))
+    end)
+
+    uv.run("once")
+
+    self:prepend(key, "1", function(self, err, ret)
+      assert(not err, tostring(err))
+      assert(ret == "STORED", tostring(ret))
+    end)
+
+    uv.run("once")
+
+    self:gets(key, function(self, err, ret, flags, cas)
+      assert(not err, tostring(err))
+      assert(ret   == "175", tostring(ret))
+      assert(flags == 12,   tostring(flags))
+      assert(type(cas) == "string",  type(cas) .. " - " .. tostring(cas))
+
+      self:cas(key, "178", cas, function(self, err, ret)
+        assert(not err, tostring(err))
+        assert(ret == "STORED", tostring(ret))
+      end)
+
+
+      self:cas(key, "177", cas, function(self, err, ret)
+        assert(not err, tostring(err))
+        assert(ret == "EXISTS", tostring(ret))
+      end)
+
+      self:get(key, function() self:close() end)
+    end)
+  end)
+
+  uv.run()
+
+end
 
 return {
   Connection = function(...) return Connection:new(...) end
 }
-
