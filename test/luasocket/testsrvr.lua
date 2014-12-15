b57c9038a3f2dcd35d859ab8054987b3636913ec
Alexey Melnichuk <mimir@newmail.ru>
2014-12-15 14:02:47 +0400

    Change. use `lluv.luasocket` to run test server.

--- test/luasocket/testsrvr.lua
+++ test/luasocket/testsrvr.lua
@@ -1,20 +1,33 @@
-socket = require("socket");
+local uv = require("lluv");
+
+socket = require("lluv.luasocket");
 host = host or "127.0.0.1";
 port = port or "8383";
-server = assert(socket.bind(host, port));
-ack = "\n";
-while 1 do
-    print("server: waiting for client connection...");
-    control = assert(server:accept());
-    while 1 do 
-        command, emsg = control:receive();
-        if emsg == "closed" then
-            control:close()
-            break
+
+local function spawn(fn, ...) coroutine.wrap(fn)(...) end
+
+local function fiber(...) uv.defer(spawn, ...) end
+
+local function main()
+    server = assert(socket.bind(host, port));
+    ack = "\n";
+    while 1 do
+        print("server: waiting for client connection...");
+        control = assert(server:accept());
+        while 1 do 
+            command, emsg = control:receive();
+            if emsg == "closed" then
+                control:close()
+                break
+            end
+            assert(command, emsg)
+            assert(control:send(ack));
+            print(command);
+            ((loadstring or load)(command))();
         end
-        assert(command, emsg)
-        assert(control:send(ack));
-        print(command);
-		((loadstring or load)(command))();
     end
 end
+
+fiber(main)
+
+uv.run()
