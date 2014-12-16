f13b5a17c7138437fce4201d8dca9c2f6d1899c2
Alexey Melnichuk <mimir@newmail.ru>
2014-12-16 11:04:16 +0400

    Change. Do not use LuaSocket library to test.

--- test/luasocket/ts.lua
+++ test/luasocket/ts.lua
@@ -1,12 +1,21 @@
-local socket = require("socket");
-local host = host or "127.0.0.1";
-local port = port or "8384";
+local uv     = require "lluv"
+local socket = require "lluv.luasocket"
+local host   = host or "127.0.0.1";
+local port   = port or "8384";
 
-server = assert(socket.bind(host, port));
+local function spawn(fn, ...) coroutine.wrap(fn)(...) end
 
-while true do
-  print("server: waiting for client connection...")
-  local control = assert(server:accept())
-  socket.sleep(0.5)
-  control:close()
-end
+local function fiber(...) uv.defer(spawn, ...) end
+
+fiber(function()
+  local server = assert(socket.bind(host, port));
+
+  while true do
+    print("server: waiting for client connection...")
+    local control = assert(server:accept())
+    socket.sleep(0.5)
+    control:close()
+  end
+end)
+
+uv.run()
