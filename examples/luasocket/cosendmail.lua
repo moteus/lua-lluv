63e881b5eb5f603ebf433b31caceb32a23445fc3
Alexey Melnichuk <mimir@newmail.ru>
2015-11-09 10:53:38 +0300

    Update example

--- examples/luasocket/cosendmail.lua
+++ examples/luasocket/cosendmail.lua
@@ -1,37 +1,25 @@
-local uv        = require "lluv"
-local ut        = require "lluv.utils"
-local socket    = require "lluv.luasocket"
-local sendmail_ = require "sendmail"
-
-local function sendmail(server, ...)
-  local s = server
-  if not ... then
-    if type(server.server) == 'string' then
-      server.server = {address = server.server}
-    end
-    s = server.server
-  end
-  assert(type(s) == "table")
-  s.create = socket.tcp;
-
-  return sendmail_(server, ...)
-end
+local uv       = require "lluv"
+local ut       = require "lluv.utils"
+local socket   = require "lluv.ssl.luasocket"
+local ssl      = require "lluv.ssl"
+local sendmail = require "sendmail"
 
 ut.corun(sendmail, {
   server = {
     address  = "localhost";
     user     = "moteus@test.localhost.com";
     password = "123456";
-    ssl      = "sslv3";
+    ssl      = ssl.context{verify = {"none"}};
+    create   = socket.ssl;
   },
 
   from = {
     title    = "Test";
-    address  = "moteus@test.localhost.com";
+    address  = "buh@intelcom-tg.ru";
   },
 
   to = {
-    address = {"alexey@test.localhost.com"}
+    address = "melnichuk@incomtel.ru";
   },
 
   message = {"CoSocket message"}
