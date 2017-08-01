935d5bc010f1ec349da455f704ca8ae9e47e5812
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2017-08-01 12:17:14 +0300

    Add. Error handler test

---
+++ test/test-error-handler.lua
@@ -0,0 +1,17 @@
+local uv = require "lluv"
+
+uv.timer():start(100, function()
+	error('SOME_ERROR_MESSAGE')
+end)
+
+local pass = false
+uv.run(function(msg)
+	print('ERROR MESSAGE:')
+	print(msg)
+	print("-------------------")
+	pass = not not string.find(msg, 'SOME_ERROR_MESSAGE', nil, true)
+end)
+
+if not pass then
+	os.exit(-1)
+end
